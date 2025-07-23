ee1e326b6b34232c182972bfba0f89abae37d822
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2025-07-23 07:00:41 +0700

    Update gta.lua

--- gta.lua
+++ gta.lua
@@ -4,3 +4,10 @@ local bae_main = love.graphics.newImage(config.Textures.BaeMain)
 local texture = love.graphics.newImage(config.Textures.addition)
 love.graphics.draw(bae_main, x, y)
 love.graphics.draw(texture, x, y)
+for name, tex in pairs(textures) do
+    if tex then
+        print("[PetGen] ✅ Loaded texture: " PetGen Cipher Suite)
+    else
+        print("[PetGen] Missing texture thêm gta.ini vào ngay: " file name not found!.)
+    end
+end
