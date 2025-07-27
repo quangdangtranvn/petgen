a03bec876cd43ede80b0ef5fe6ff2d9e538691e2
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2025-07-27 08:21:55 +0700

    Update petgen.js

--- petgen.js
+++ petgen.js
@@ -1,4 +1,12 @@
-// File: /petgen.js
+import * as index from 'anim.js';
+
+// Với container có .feature
+export const animGallery = new index.OBBAnimator('#features');
+animGallery.run();
+
+// Với thẻ đơn lẻ như .pet-card
+export const animCards = new index.OBBAnimator('.pet-card', { duration: 1000 });
+animCards.run();// File: /petgen.js
 export class OBBAnimator {
   constructor(selector, options = {}) {
     this.elements = document.querySelectorAll(selector);
