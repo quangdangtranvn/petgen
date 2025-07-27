758d1d87502c152a847934ebf021e21504339d5d
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2025-07-27 08:51:30 +0700

    Update ecs.js

--- ecs.js
+++ ecs.js
@@ -1,5 +1,32 @@
+// ECS struct-like class in JS mimicking C# style
+class ECS {
+  constructor(options = {}) {
+    this.id = options.id || 'growLangMixture';
+    this.enabled = options.enabled ?? false;
+    this.label = options.label || 'Grow Lang Mixture';
+this.cube = [0,0,0];
+this.box = [0,0,0,0];
+this.isPointed = true;
+this.isSimulating = true;
+  }
+
+  toggle() {
+    this.enabled = !this.enabled;
+  }
+
+  toPrompt() {
+    return this.enabled ? '+growLangMixture' : '';
+  }
+}
+
+// Khởi tạo ECS
+const growLangBox = new ECS({ enabled: false });
 class ECS {
   constructor() {
+this.isPointed = true;
+this.isSimulating = true;
+this.box = [1,1,1,1];
+this.cube = [1,1,1];
     this.checkbox = document.getElementById('growLangMixture');
     this.init();
   }
