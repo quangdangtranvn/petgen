c92d8a33c054c22de78c0fc38cf044c6a5ff8eac
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2025-08-03 04:03:40 +0700

    Create file.sql

---
+++ file.sql
@@ -0,0 +1,18 @@
+-- Burn an NFT
+UPDATE nft
+SET owner_address = '0x0000000000000000000000000000000000000000',
+    status = 'burned'
+WHERE token_id = 101;
+
+-- Freeze an NFT
+UPDATE nft
+SET status = 'frozen'
+WHERE token_id = 102;
+
+LOAD DATA INFILE '/storage/emulated/0/Download/petgen-main/view/wallets.csv'
+INTO TABLE pagma_nfts
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(token_id, owner_address, metadata_url, traits, status);
