2ffdf2ca44e26cafa2aa950ade820a7900a5903d
quangdangtranvn <93366525+quangdangtranvn@users.noreply.github.com>
2025-08-07 05:49:28 +0700

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,9 @@
+# sub/connector.py
+
+def connect():
+    print("🔌 Đã kết nối từ module phụ!")
+    return "Kết nối thành công"
+
+class SubProcessor:
+    def run(self):
+        print("⚙️ Đang xử lý trong SubProcessor...")
